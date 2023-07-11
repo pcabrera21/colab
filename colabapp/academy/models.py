466689bca572e08069193a87d7817c2952a5a8b6
Pedro Cabrera <pedro.cabrera.garcia@gmail.com>
2023-07-11 01:54:17 +0000

    modelos creados

--- colabapp/academy/models.py
+++ colabapp/academy/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    email = models.CharField(max_length=128)
+    bio = models.CharField(max_length=500)
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+    email = models.CharField(max_length=128)
+
+class Course(models.Model):
+    name = models.CharField(max_length=128)
+    desription = models.CharField(max_length=500)
+
+class Subject(models.Model):
+    course = models.ForeignKey(Course, on_delete = models.PROTECT) # = models.CASCADE "elimina todo" // = models.PROTECT "protege la eliminacion"
+    teacher = models.ForeignKey(Teacher, on_delete = models.PROTECT)
+    start_date = models.DateField()
+
+class Subscription(models.Model):
+    subject = models.ForeignKey(Subject, on_delete = models.PROTECT)
+    student = models.ForeignKey(Student, on_delete = models.PROTECT)
