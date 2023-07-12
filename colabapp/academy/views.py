2cc76b2e621f4c58ac40a176dca26abdd775baeb
Pedro Cabrera <pedro.cabrera.garcia@gmail.com>
2023-07-12 01:50:41 +0000

    url cursos creado

--- colabapp/academy/views.py
+++ colabapp/academy/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from .models import Course
 
 # Create your views Academy.
 
 def v_index(request):
-    context = {}
+    context = {
+        'course1': Course.objects.get(name = 'BD1'),
+        'course2': Course.objects.get(name = 'BD2')
+    }
     return render(request, 'index.html', context) #Enlaza la view de Academy con el html
 
-def v_course(request):
-    context = {}
+def v_course(request, course_id):
+    context = {
+        'course': Course.objects.get(id = course_id)
+    }
     return render(request, 'course.html', context)
